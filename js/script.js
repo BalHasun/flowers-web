f0bf688fb4864051b59f4c51724d583eecf20ffc
Bal Elsada Hasun <fouadhason@gmail.com>
2022-04-13 17:24:59 +0300

    The first addition to the site

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function(){
+
+
+//  landing section
+var mySwiper = new Swiper(".swiper-container", {
+	direction: "vertical",
+	loop: true,
+	pagination: ".swiper-pagination",
+	grabCursor: true,
+	speed: 1000,
+	paginationClickable: true,
+	parallax: true,
+	autoplay: false,
+	effect: "slide",
+	// mousewheelControl: 1
+});
+//  landing section
+
+
+
+})
